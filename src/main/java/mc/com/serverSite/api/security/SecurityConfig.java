74c08084c9936ed4565852761eb11c698c863aa7
Rodr1goTavares <rodr1gowzxz2@gmail.com>
2025-03-15 19:21:21 -0300

    :construction: Security config and properties file fix.

--- src/main/java/mc/com/serverSite/api/security/SecurityConfig.java
+++ src/main/java/mc/com/serverSite/api/security/SecurityConfig.java
@@ -1,4 +1,77 @@
 package mc.com.serverSite.api.security;
 
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
+
+    private final JwtTokenFilter jwtTokenFilter;
+
+    public SecurityConfig(JwtTokenFilter jwtTokenFilter) {
+        this.jwtTokenFilter = jwtTokenFilter;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                //.csrf((csrf) -> csrf.ignoringRequestMatchers("/***"))
+                .csrf(AbstractHttpConfigurer::disable)
+                .logout((logout) ->
+                        logout
+                                .deleteCookies("JSESSIONID")
+                                .clearAuthentication(true)
+                                .invalidateHttpSession(true)
+                                .logoutUrl("/auth/logout")
+                                .logoutSuccessUrl("/")
+                )
+                .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(
+                        (authorizeConfig) -> {
+                            authorizeConfig
+                                    .requestMatchers("/**").permitAll() // <- For development only. Remove this line on deploy.
+                                    .requestMatchers("/dashboard").authenticated()
+                                    .requestMatchers("/admin/**").hasRole("ADMIN")
+                                    .anyRequest().authenticated();
+                        }
+                )
+                .addFilterBefore(this.jwtTokenFilter, UsernamePasswordAuthenticationFilter.class)
+                .logout(LogoutConfigurer::permitAll);
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public LogoutSuccessHandler logoutSuccessHandler() {
+        return new HttpStatusReturningLogoutSuccessHandler(HttpStatus.OK);
+    }
+
 }
--- src/main/java/mc/com/serverSite/service/auth/verification/ways/VerificationGateway.java
+++ src/main/java/mc/com/serverSite/service/auth/verification/ways/VerificationGateway.java
@@ -3,7 +3,6 @@ package mc.com.serverSite.service.auth.verification.ways;
 public interface VerificationGateway {
 
     /**
-
      * @param recipient Receiver (email or phone number)
      * */
     void sendVerification(String recipient, String otpCode);
